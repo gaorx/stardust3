60bbfe87063755d9a39403f56a3f4cd2175a4e40
Gao Rongxin <rongxin.gao@gmail.com>
2021-08-05 22:57:25 +0800

    developing

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gaorx/stardust3
 
 go 1.16
 
-require github.com/pkg/errors v0.9.1 // indirect
+require (
+	github.com/hashicorp/go-multierror v1.1.1 
+	github.com/pkg/errors v0.9.1 
+)
--- sderr/export.go
+++ sderr/export.go
@@ -1,6 +1,7 @@
 package sderr
 
 import (
+	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
 )
 
@@ -8,6 +9,7 @@ var (
 	// New
 	New    = errors.New
 	Errorf = errors.Errorf
+	Prefix = multierror.Prefix
 
 	// With
 	WithStack    = errors.WithStack
@@ -23,9 +25,37 @@ var (
 	// Is/As
 	Is = errors.Is
 	As = errors.As
+
+	// Multiple
+	Append     = multierror.Append
+	Flatten    = multierror.Flatten
+	ListFormat = multierror.ListFormatFunc
 )
 
 type (
-	Frame      = errors.Frame
-	StackTrace = errors.StackTrace
+	Frame           = errors.Frame
+	StackTrace      = errors.StackTrace
+	MultipleError   = multierror.Error
+	ErrorFormatFunc = multierror.ErrorFormatFunc
+	Group           = multierror.Group
 )
+
+func Select(err1, err2 error) error {
+	if err1 == nil && err2 == nil {
+		return nil
+	} else if err1 != nil && err2 != nil {
+		return Append(err1, err2)
+	} else if err1 != nil {
+		return err1
+	} else {
+		return err2
+	}
+}
+
+func Concurrently(funcs ...func() error) *MultipleError {
+	var g Group
+	for _, f := range funcs {
+		g.Go(f)
+	}
+	return g.Wait()
+}
